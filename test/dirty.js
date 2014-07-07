949f20af0c39ed0528628bca0d4f5f0ff2f9ccfc
Yomguithereal <guillaumeplique@gmail.com>
2014-07-07 17:02:15 +0200

    Example

--- test/dirty.js
+++ test/dirty.js
@@ -1,10 +1,12 @@
 var sandcrawler = require('sandcrawler');
 
 sandcrawler.scrape('https://news.ycombinator.com/', function() {
+
   artoo.scrape('td.title:has(a):not(:last)', {
     title: {sel: 'a'},
     url: {sel: 'a', attr: 'href'}
   }, artoo.done);
-}).done(function(data) {
+
+}).then(function(data) {
   // Write data to file or else...
 });
